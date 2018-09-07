0d354972ade8e7305446a09bfb06ecb53f0f1d5a
Eric Lopez <ericlopez07@gmail.com>
2018-09-07 07:30:25 -0600

    first commit

--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -1,11 +1,18 @@
 <?php require_once('../../../private/initialize.php'); ?>
+<?php
+  $subjects = [
 
+    ['id'] => '1', 'postion'
+  ]
 <?php $page_title = 'Subjects'; ?>
 
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
     <div id="content">
-
+      <div id='main-menu'>
+        <h2>Main Menu</h2>
+        <ul>
+          <li>
     </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
